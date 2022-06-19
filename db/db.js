b614112e4bb5b4cae9fd7b54e682067ab255579a
rabin245 <drarabin@gmail.com>
2022-06-19 16:18:20 +0545

    check connection at start

--- db/db.js
+++ db/db.js
@@ -2,4 +2,14 @@ const knex = require("knex");
 const knexfile = require("../knexfile");
 
 const db = knex(knexfile.development);
+
+db.raw("SELECT 1")
+  .then(() => {
+    console.log("Database connected successfully...");
+  })
+  .catch((e) => {
+    console.log("Error while connecting to the database...");
+    console.error(e);
+    process.exit(1);
+  });
 module.exports = db;
